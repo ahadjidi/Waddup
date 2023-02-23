4f17e479c65a5dd62bc8accc72e32505139c25ab
shbradki <91555214+shbradki@users.noreply.github.com>
2023-02-22 22:02:44 -0500

    fixed urls

--- abstract-user-example/users/urls.py
+++ abstract-user-example/users/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 from . import views
 
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("users/", include("django.contrib.auth.urls")),
+    path("users/signup/", views.SignUp.as_view(), name="signup"),
+    
     path('happ-now/', views.EventList.as_view(), name = 'waddup_home'),
     path('logout/', views.EventList.as_view(),name='waddup_home'),
     path('<slug:slug>/', views.EventDetail.as_view(), name = 'event_detail'),
-    path("signup/", views.SignUp.as_view(), name="signup"),
+    #path("signup/", views.SignUp.as_view(), name="signup"),
     ]
