12cba48a1b68886ee3c7a720a0bc7fbe9cfc7c53
jehandra <105846660+jillianbeann@users.noreply.github.com>
2023-03-01 23:11:19 -0500

    post working

--- abstract-user-example/users/models.py
+++ abstract-user-example/users/models.py
@@ -61,7 +61,7 @@ class Event(models.Model):
 
     # def __str__(self):
     #     return self.title
-    #
+
     def get_absolute_url(self):
         return reverse('waddup_home')
     class Meta:
